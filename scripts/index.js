7bf86827b35e6f357268bb2d48d7b6f9fae75a92
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-03-11 17:32:17 -0500

    Solucionado errores

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,16 @@ window.onload = function (){
 
     fetchPosts('mijel.dev')
         .then(data => {
-            console.log(data.items)
             data.items.map(post => {
                 const articleContainer = document.createElement('div')
                 articleContainer.classList.add('article')
-                
+
                 articleContainer.innerHTML = `
                     <img src="./statics/images/mmmmmmfood-historia 1.png" alt="Article Image">
                     <div>
                         <h1>${post.title}</h1>
-                        <a href="${post.link}">Ver artículo completo</a>
-                        <h4>02 de setiembre</h4>
+                        <a href="${post.link}" target="_blank">Ver artículo completo</a>
+                        <h4>Fecha: ${post.pubDate}</h4>
                     </div>
                 `
 
@@ -28,6 +27,5 @@ const fetchPosts = async(user) => {
     const api  = `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${user}` 
     const resp = await fetch(api)
     const data = await resp.json()
-
     return data
 }
